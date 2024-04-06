09169086da02d983fb4b0d1d107a8ce9da796cdf
Ian Kincaid <i.s.kincaid@gmail.com>
2024-04-05 20:16:08 -0400

    Initial Commit

---
+++ Exercise Calorie Counter (SwiftUI)/ContentView.swift
@@ -0,0 +1,154 @@
+//
+//  ContentView.swift
+//  Exercise Calorie Counter (SwiftUI)
+//
+//  Created by Ian Kincaid on 4/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var selection = 0
+    
+    @State var age: String = "20"
+    @State var weight: String = "75"
+    @State var gender: String = "Female"
+    @State var exercise_time: String = "0"
+    @State var avg_heart_rate: String = "70"
+    @State var calories_text: String = "... calories burned."
+    
+    var body: some View {
+        TabView(selection: $selection) {
+            Form {
+                VStack {
+                    
+                    Text("Enter Personal Info")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .padding(.bottom, 50)
+                    
+                    HStack {
+                        Text("Your age:")
+                            .font(.title)
+                        TextField("Enter your age here", text: self.$age)
+                            .font(.title)
+                    }
+                    
+                    HStack {
+                        Text("Your weight:")
+                            .font(.title)
+                        TextField("Enter your weight here", text: self.$weight)
+                            .font(.title)
+                    }
+                    .padding(.bottom, 50)
+                    
+                    Picker(selection: $gender, label: Text("Please select your gender")) {
+                        Text("Female").tag("Female")
+                        Text("Male").tag("Male")
+                    }
+                            .pickerStyle(SegmentedPickerStyle())
+                            .padding(.bottom, 50)
+                    
+                    Text("Please advance to the next tab to enter your exercise info...")
+                        .font(.title)
+                        .background(Color.yellow)
+                        .foregroundColor(Color.blue)
+                    }
+                }
+            
+            .tabItem{
+                VStack {
+                    Image("first")
+                    Text("enter personal info")
+                }
+            }
+            .tag(0)
+            
+            Form {
+                VStack {
+                    
+                    Text ("Enter Exercise Info")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .padding(.bottom, 50)
+                    
+                    HStack {
+                        Text("Exercise Time:")
+                            .font(.largeTitle)
+                        TextField("Enter your exercise time here", text: self.$exercise_time)
+                            .font(.title)
+                    }
+                    
+                    HStack {
+                        Text("Heart Rate:")
+                            .font(.title)
+                        TextField("Enter your average heart rate here", text: self.$avg_heart_rate)
+                            .font(.title)
+                    }
+                    .padding(.bottom, 50)
+                    
+                    Text("Please advance to the next tab to calculate the calories...")
+                        .font(.title)
+                        .background(Color.yellow)
+                        .foregroundColor(Color.blue)
+                }
+            }
+            
+            .tabItem {
+                VStack {
+                    Image("second")
+                    Text("Enter exercise info")
+                }
+            }
+            .tag(1)
+            
+            VStack {
+                Text("Display Results")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .padding(.bottom, 50)
+                
+                Button(action: {
+                    
+                    let weight_float = Float(self.weight)
+                    let age_float = Float(self.age)
+                    let heart_rate_float = Float(self.avg_heart_rate)
+                    let exercise_time_float = Float(self.exercise_time)
+                    var caloriesBurned: Float = 0.0
+                    
+                    switch self.gender {
+                    case "Female":
+                        caloriesBurned = ((age_float! * 0.074) - (weight_float! * 0.05741) + (heart_rate_float! * 0.4472) - 20.4022) * exercise_time_float! / 4.184
+                    case "Male":
+                        caloriesBurned = ((age_float! * 0.2017) - (weight_float! * 0.09036) + (heart_rate_float! * 0.6309) - 55.0969) * exercise_time_float! / 4.184
+                    default:
+                        return
+                    }
+                    
+                    self.calories_text = String(Int( caloriesBurned )) + " calories burned"
+                }) {
+                    Text("Click here to calculate")
+                        .font(.title)
+                }
+                .padding(.bottom, 50)
+                
+                Text(self.calories_text)
+                    .font(.title)
+            }
+            
+            .tabItem {
+                VStack {
+                    Image("first")
+                    Text("Calculate calories")
+                }
+            }
+            .tag(2)
+        }
+    }
+                
+}
+
+#Preview {
+    ContentView()
+ }
---
+++ Exercise Calorie Counter (SwiftUI)/Exercise_Calorie_Counter__SwiftUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  Exercise_Calorie_Counter__SwiftUI_App.swift
+//  Exercise Calorie Counter (SwiftUI)
+//
+//  Created by Ian Kincaid on 4/5/24.
+//
+
+import SwiftUI
+
+@main
+struct Exercise_Calorie_Counter__SwiftUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
